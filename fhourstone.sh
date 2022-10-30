a9e5843420e1b7baf443b9ec3e11a1e30c935362
JakeLynas <103260779+JakeLynas@users.noreply.github.com>
2022-10-30 17:37:29 +0000

    Create fhourstone.sh
    bash script for fhourstone benchmark (requires git install)

---
+++ fhourstone.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir fhourstones
+cd fhourstones
+git clone https://github.com/qu1j0t3/fhourstones.git
+cd fhourstones
+gcc -O3 SearchGame.c -o SearchGame
+./SearchGame < inputs > fhourstones_test.dat
